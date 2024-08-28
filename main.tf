4ca96b270221462d3cc624a25d2e26e271227a84
anil kumar <anilgpkumar@gmail.com>
2024-08-29 03:49:17 +0530

    this is my 4th commit

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "example" {
   name     = "eanil"
   location = "West Europe"
 }
+
+
+resource "azurerm_resource_group" "example2" {
+  name     = "eanil2"
+  location = "West Europe"
+}
+
+
